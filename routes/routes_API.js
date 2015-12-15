a1450d4f8eca1407e359a557d3da1e9bc4d31055
Xchel_Sanchez <xchelsvz@gmail.com>
2015-12-15 11:07:54 -0600

    Separate API

--- routes/routes_API.js
+++ routes/routes_API.js
@@ -71,62 +71,6 @@ var  User = require('../Models/user_model');
 		})
 
 
-	app.route('/api/login')
-
-		.post(function (request,response){
-
-			
-			User.findOne({name : request.body.name}, function (err , user){
-				if (err) throw err;
-				
-				if(user != undefined){
-					user.comparePassword(request.body.pass, function (err , pass){
-						if (err) throw err;
-
-							if(pass){
-								
-								var UIDSession = uuid.v4();
-								request.session._id = UIDSession;
-								RedisClient.set(UIDSession , user._id, function (err, value){
-									console.log('Token de Session: '+value);
-								});
-								RedisClient.expire(UIDSession,3600);
-								response.send(UIDSession);
-								
-							}
-							else{
-								
-								response.sendStatus(404);
-							}
-					})
-				}else
-				{
-	
-					response.sendStatus(404);
-					
-				}
-
-			})
-
-		})
-		
-
-	app.route('/api/logout')
-
-		.post(function (request, response){
-
-			
-			request.session.destroy(function (err){
-
-				RedisClient.del(request.body.token, function (err,reply){
-					response.sendStatus(200);
-				})
-
-			})
-
-		})
-
-
 	app.route('/logout')
 
 		.get(function (request, response){
@@ -171,44 +115,6 @@ var  User = require('../Models/user_model');
 			
 		})
 
-	app.route('/api/user')
-
-		.get(function (request, response){ 	
-
-
-		})
-
-		.post(function (request, response){
-
-			RedisClient.exists(request.body.token, function (err, reply){
-
-				if(reply===1){
-			//if (request.session._id){
-				User.find('',function (err, docs){
-					if (err) throw err;
-					console.log(docs);
-					response.send(docs);
-				})
-			} else {
-				response.send(404);
-			}
-			/*}
-			else{
-				request.session.destroy(function (err){
-					response.sendStatus(404);
-				})
-			}*/
-			});
-
-		})
-
-		.put(function (request, response){
-
-		})
-
-		.delete(function (request, response){
-			
-		})
 
 
 	app.route('/user')
