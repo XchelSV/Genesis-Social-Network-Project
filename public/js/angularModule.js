c0e323bb59ff0dad470cfb451b2dac4130b4a1ce
Xchel_Sanchez <xchelsvz@gmail.com>
2015-12-16 14:54:32 -0600

    Post Front End Function

--- public/js/angularModule.js
+++ public/js/angularModule.js
@@ -1,6 +1,48 @@
 var app = angular.module('Genesis',['ngRoute', 'ngCookies']);
+	
+	app.directive('fileModel', ['$parse', function ($parse) {
+	   	 return {
+	        restrict: 'A',
+	        link: function(scope, element, attrs) {
+	            var model = $parse(attrs.fileModel);
+	            var modelSetter = model.assign;
+	            
+	            element.bind('change', function(){
+	                scope.$apply(function(){
+	                    modelSetter(scope, element[0].files[0]);
+	                });
+	            });
+	        }
+	    };
+	}]);
 
-	app.controller('indexController',function  ($scope, $http, $cookies) {
+	app.service('fileUpload', ['$http', function ($http) {
+	    this.uploadFileToUrl = function(file,id,text,img,video,audio, uploadUrl){
+	        var fd = new FormData();
+	        fd.append('file', file);
+	        fd.append('id',id);
+	        fd.append('body',text);
+	        fd.append('img',img);
+	        fd.append('video',video);
+	        fd.append('audio',audio);
+
+	        var date = new Date();
+	        fd.append('date',date);
+
+	        $http.post(uploadUrl, fd, {
+	            transformRequest: angular.identity,
+	            headers: {'Content-Type': undefined}
+	        })
+	        .success(function(){
+
+
+	        })
+	        .error(function(){
+	        });
+	    }
+	}]);
+	
+	app.controller('indexController',function  ($scope, $http, $cookies, fileUpload) {
 		
 		$scope.session = function(){
 			if($cookies.session != undefined){
@@ -16,6 +58,45 @@ var app = angular.module('Genesis',['ngRoute', 'ngCookies']);
 		$scope.month = decodeURIComponent($cookies.month);
 		$scope.servicePlace = decodeURIComponent($cookies.servicePlace);
 		$scope.biography = decodeURIComponent($cookies.biography);
+
+		$scope.animation = false;
+
+		$scope.post = function (){
+
+			if($scope.postText == '' && $scope.myFile == undefined){
+
+			}
+			else{
+
+				if ($scope.myFile != undefined){
+
+					var file = $scope.myFile;
+					var uploadUrl = '/posts';
+					fileUpload.uploadFileToUrl(file,$scope.id, $scope.postText, true,false,false ,uploadUrl);
+
+						$scope.postText = '';
+						
+						var d = document.getElementById("myModal");
+						d.className = d.className + "bounceOut";
+
+					
+
+				}else{
+
+					var file = $scope.myFile;
+					var uploadUrl = '/posts';
+					fileUpload.uploadFileToUrl(file,$scope.id, $scope.postText, false,false,false ,uploadUrl);
+
+						$scope.postText = '';
+						
+						var d = document.getElementById("myModal");
+						d.className = d.className + "bounceOut";
+
+					
+				}
+			}
+
+		}
 	});
 
 	app.controller('addUserController',function  ($scope, $http, $cookies) {
